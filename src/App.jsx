8a99cd8d40bc13e159375de0c213e1d601e98313
Smith <bsmith672@rku.ac.in>
2024-01-06 23:11:25 +0530

    style completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="wholebody">
+      <div className="clock">
+        <div style={{'--clr':'white'}} className="hand">
+          <i></i>
+        </div>
+        <div style={{'--clr':'red'}} className="hand">
+          <i></i>
+        </div>
+        <div style={{'--clr':'yellow'}} className="hand">
+          <i></i>
+        </div>
+        <span style={{ "--i": 1 }}>
+          <b>1</b>
+        </span>
+        <span style={{ "--i": 2 }}>
+          <b>2</b>
+        </span>
+        <span style={{ "--i": 3 }}>
+          <b>3</b>
+        </span>
+        <span style={{ "--i": 4 }}>
+          <b>4</b>
+        </span>
+        <span style={{ "--i": 5 }}>
+          <b>5</b>
+        </span>
+        <span style={{ "--i": 6 }}>
+          <b>6</b>
+        </span>
+        <span style={{ "--i": 7 }}>
+          <b>7</b>
+        </span>
+        <span style={{ "--i": 8 }}>
+          <b>8</b>
+        </span>
+        <span style={{ "--i": 9 }}>
+          <b>9</b>
+        </span>
+        <span style={{ "--i": 10 }}>
+          <b>10</b>
+        </span>
+        <span style={{ "--i": 11 }}>
+          <b>11</b>
+        </span>
+        <span style={{ "--i": 12 }}>
+          <b>12</b>
+        </span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
