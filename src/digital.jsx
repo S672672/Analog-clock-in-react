0fc356e4ef04480a38523f10be87c9f2093a1d19
Smith <bsmith672@rku.ac.in>
2024-01-07 15:30:14 +0530

    date also added

--- src/digital.jsx
+++ src/digital.jsx
@@ -1,12 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import './App.css'
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 export default function Digital() {
   const [currentTime, setCurrentTime] = useState({
     hour: new Date().getHours(),
     min: new Date().getMinutes(),
     sec: new Date().getSeconds(),
-    ampm:new Date() < 12 ? 'AM' : 'PM',
+    day: new Date().toLocaleDateString('en-US', { weekday: 'long' }),
+    month: new Date().toLocaleDateString('en-US', { month: 'long' }),
+    date:new Date().getDate(),
+    year: new Date().getFullYear(),
+    ampm: new Date() < 12 ? "AM" : "PM",
   });
 
   useEffect(() => {
@@ -16,7 +20,11 @@ export default function Digital() {
         hour: newTime.getHours(),
         min: newTime.getMinutes(),
         sec: newTime.getSeconds(),
-        ampm: newTime < 12 ? 'AM' : 'PM',
+        day: newTime.toLocaleDateString('en-US', { weekday: 'long' }),
+        month: newTime.toLocaleDateString('en-US', { month: 'long' }),
+        date:newTime.getDate(),
+        year: newTime.getFullYear(),
+        ampm: newTime < 12 ? "AM" : "PM",
       });
     }, 1000);
 
@@ -24,13 +32,21 @@ export default function Digital() {
   }, []);
 
   return (
-    <div className='digital'>
-      <div className='text-yellow-400'>{currentTime.hour}</div>
-      <div className='text-white'>:</div>
-      <div className='text-red-500 '>{currentTime.min}</div>
-      <div className='text-white '>:</div>
-      <div className='text-blue-500 '>{currentTime.sec}</div>
-      <div className='text-white'>{currentTime.ampm}</div>
-      </div>
+    <div className="wholedate">
+    <div className="digital">
+      <div className="text-yellow-400">{currentTime.hour}</div>
+      <div className="text-white">:</div>
+      <div className="text-red-500 ">{currentTime.min}</div>
+      <div className="text-white ">:</div>
+      <div className="text-blue-500 ">{currentTime.sec}</div>
+      <div className="text-white">{currentTime.ampm}</div>
+    </div>
+    <div className="dateonly">
+    <div>{currentTime.day}, </div>
+    <div>{currentTime.date},</div>
+    <div>{currentTime.month},</div>
+    <div>{currentTime.year}</div>
+    </div>
+    </div>
   );
 }
